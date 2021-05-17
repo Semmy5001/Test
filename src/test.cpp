0a4e9d8bcd3ffee0101da19cff26def7b08415bd
Themistoklis Foukis <themistoklis.foukis5000@gmail.com>
2021-05-17 04:42:27 +0200

    First Commit

---
+++ src/test.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Themistoklis Foukis
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Ich bin ein TEst" << endl; // prints !!!Hello World!!!
+
+	for (int i = 0; i<= 10; i++){
+		cout << "For schlexcxccxcife!" << endl;
+	}
+	return 0;
+}
